ac6c4a716ab748b2f987374eb6910cf758714a46
Bridger Larsen <bridgerjimmy@gmail.com>
2021-06-03 12:09:54 -0600

    added and finished the availability and contact pages styles and all

--- src/components/pages/availability.js
+++ src/components/pages/availability.js
@@ -44,6 +44,82 @@ class Availability extends Component {
                     buttonHeader="Questionnaire"
                     path="/questionnaire"
                 />
+
+                <div className="availability-purchase-wrapper">
+                    <div className="purchase-info-wrapper">
+                        <h1 className="purchase-heading">
+                            Purchasing a Puppy from Us
+                        </h1>
+
+                        <p className="purchase-detail">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, ab quo reprehenderit rem odio corrupti in veritatis iure deleniti, corporis, quis aspernatur non ipsa numquam.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem minus rem, beatae blanditiis tempore eligendi amet perspiciatis iste numquam laborum, tempora, magnam qui? Officiis nesciunt pariatur facilis enim unde accusantium vitae voluptas sit.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae inventore id excepturi culpa iusto cum commodi molestias harum qui, sit architecto ratione.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis delectus dolor molestias quos voluptatum assumenda vero accusamus pariatur quaerat.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere sapiente fugit ad illo delectus repellendus!</p>
+
+                        <img className="purchase-detail-img" src="http://via.placeholder.com/480x720" />
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sequi culpa veniam debitis minus facere fugit maxime sed vel! Inventore similique natus officiis aperiam.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur vero culpa, eos cumque iure fugit est alias facere!</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure delectus, deleniti commodi harum officiis, placeat quae suscipit perspiciatis sint cupiditate mollitia minus laudantium officia odio culpa!</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut ipsum adipisci</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, repellendus reprehenderit id aut, error tempora sapiente voluptatibus sequi debitis, neque cumque.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sint quos perspiciatis. Nesciunt voluptates corporis ex aut rem recusandae rerum dolores! Ut ea earum veritatis, blanditiis nesciunt velit voluptate sunt saepe.</p>
+
+                        <p className="purchase-detail">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores?</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda odio, quis nihil velit ducimus, consectetur vero cupiditate nesciunt dolorum porro voluptas. Quis exercitationem cumque ex esse accusantium corrupti iure temporibus atque quas quam commodi tenetur veritatis vero repudiandae, quasi hic itaque.</p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nemo debitis blanditiis? Dolorem laudantium ipsum facilis modi cum, magnam nihil. Inventore, fugiat. Iusto, possimus dolor veniam labore nam impedit totam amet veritatis nostrum consequatur tenetur! Impedit esse iure provident temporibus! Labore ea veritatis maiores totam dolore minima reiciendis quod, asperiores commodi magnam magni temporibus beatae perferendis explicabo corrupti.</p>
+
+                        <p className="purchase-detail-points">
+                            <h3 className="detail-heading">All Aussies come with:</h3>
+
+                            <div className="detail-points">
+                                <ul>
+                                    <li>Registration Papers</li>
+                                    <li>Contract</li>
+                                    <li>Deworming at 2, 4, 6 & 8 weeks of age</li>
+                                    <li>Dew Claws Removed</li>
+                                    <li>Testing</li>
+                                    <li>First Set of Shots</li>
+                                    <li>Leash</li>
+                                    <li>Collar</li>
+                                </ul>
+
+                                <ul>
+                                    <li>Proof of vaccinations from a licensed veterinarian</li>
+                                    <li>Letter stating excellent health from a licensed veterinarian</li>
+                                    <li>Parents Genetics</li>
+                                    <li>Negative fecal test</li>
+                                    <li>Tail Docked</li>
+                                </ul>
+
+                                <ul>
+                                    <li>Toy</li>
+                                    <li>Food</li>
+                                    <li>Puppy pads</li>
+                                    <li>Wipes</li>
+                                    <li>Potty bags</li>
+                                    <li>Paper Towels</li>
+                                    <li>And much more</li>
+                                </ul>
+                            </div>
+                        </p>
+
+                        <p className="purchase-detail">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid esse voluptas architecto doloribus earum nobis quidem voluptate explicabo eius incidunt temporibus possimus id ex, distinctio, eaque quibusdam cumque, maxime corrupti.</p>
+
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import HeaderImage from '../headerImage';
 
 class Contact extends Component {
     render() {
         return (
             <div className="contact-us-container">
-                contact page is here
+                <HeaderImage 
+                    img={"http://via.placeholder.com/1000x1000"} 
+                    className="contact-img" 
+                    height="480px"
+                    title="Contact Us"
+                />
+
+                <div className="contact-information-wrapper">
+                    <div className="left-column">
+                        <h4 className="information-name">
+                            Kota's Arrow
+                        </h4 >
+
+                        <h4 className="information-number">
+                            Phone: 888-888-8888
+                        </h4>
+
+                        <h4 className="information-email">
+                            Email: kotasArrow@example.com
+                        </h4>
+
+                        <h4 className="information-location">
+                            Alpine, Utah
+                        </h4>
+                    </div>
+
+                    <div className="right-column">
+                        <p className="contact-detail">To Learn more about our dogs, please contact us!</p>
+
+                        <NavLink 
+                            to="/faq" 
+                            className="contact-faq-link"
+                        >
+                            Please check our FAQ page for answers to commonly asked questions.
+                        </NavLink>
+                    </div>
+                </div>
             </div>
         )
     }
