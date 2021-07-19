79ee2a20e8645d4b8ca0f2dfa2b125064c56c156
Bridger Larsen <bridgerjimmy@gmail.com>
2021-07-19 15:35:00 -0600

    Edited the no-match page to be simple and mobile resposive

--- src/components/pages/noMatch.js
+++ src/components/pages/noMatch.js
@@ -5,24 +5,18 @@ import Button from '../buttons';
 export default function NoMatch() {
     return (
         <div className="no-match-page-wrapper">
-            <div className="left-side">
-                <div className="headings-wrapper">
-                    <h1 className="code-heading">
-                        404
-                    </h1>
+            <div className="headings-wrapper">
+                <h1 className="code-heading">
+                    404
+                </h1>
 
-                    <h1 className="detail-heading">
-                        Sorry, page could not be found
-                    </h1>
-                </div>
-
-                <div className="btn-wrapper">
-                    <Button className="no-match-button" path="/" buttonHeader="Return to Homepage" />
-                </div>
+                <h1 className="detail-heading">
+                    Sorry, page could not be found
+                </h1>
             </div>
 
-            <div className="right-side">
-                <img alt="no match" src="http://via.placeholder.com/1000x1000" />
+            <div className="btn-wrapper">
+                <Button className="no-match-button" path="/" buttonHeader="Return to Homepage" />
             </div>
         </div>
     )
