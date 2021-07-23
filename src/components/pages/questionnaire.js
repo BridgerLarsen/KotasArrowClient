c3a4c6b63e4b017278e42c72a2138624cccde34f
Bridger Larsen <bridgerjimmy@gmail.com>
2021-07-23 12:09:06 -0600

    Made google survey in questionarie component to dynamically change size for mobile responsiveness

--- src/components/pages/questionnaire.js
+++ src/components/pages/questionnaire.js
@@ -1,34 +1,69 @@
-import React from "react";
+import React, { Component } from "react";
 
 import HeaderImage from '../headerImage'; 
 
 import QuestionnaireHeaderImage from "../../static/assests/images/lightColoredAussie.jpg";
 
-const Questionnaire = () => {
-    return (
-        <div className="questionnaire-container">
-            <HeaderImage 
-                img={QuestionnaireHeaderImage}
-                title="Questionnaire"
-                className="questionnaire-page-image"
-                height="480px"
-            />
+class Questionnaire extends Component {
+    constructor() {
+        super();
 
-            <div className="form-wrapper">
-                <iframe 
-                    src="https://docs.google.com/forms/d/e/1FAIpQLSddtlyfZQr6qiQn8nTgRJx2-ZA2jfAEl5ILF_8d9nlIBK6x7A/viewform?embedded=true"  
-                    title="Google Survey"
-                    width="675px"
-                    height="750px"
-                    frameBorder="0" 
-                    marginHeight="0" 
-                    marginWidth="0"
-                >
-                    Loading…
-                </iframe>
+        this.state = {
+            documentWidth: "675px"
+        }
+
+        window.addEventListener("resize", () => {
+            this.onResize();
+        }, false);
+        this.onResize = this.onResize.bind(this);
+    }
+
+    onResize() {
+        if (document.documentElement.offsetWidth <= 675) {
+            this.setState({
+                documentWidth: "345px"
+            })
+        } else if (document.documentElement.offsetWidth >= 676) {
+            this.setState({
+                documentWidth: "675px"
+            })
+        }
+    }
+
+    componentDidMount() {
+        this.onResize();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.onResize, false);
+    }
+    render() {
+        return (
+            <div className="questionnaire-container">
+                <HeaderImage 
+                    img={QuestionnaireHeaderImage}
+                    title="Questionnaire"
+                    className="questionnaire-page-image"
+                    height="480px"
+                />
+    
+                <div className="form-wrapper">
+                    <iframe 
+                        src="https://docs.google.com/forms/d/e/1FAIpQLSddtlyfZQr6qiQn8nTgRJx2-ZA2jfAEl5ILF_8d9nlIBK6x7A/viewform?embedded=true"  
+                        title="Google Survey"
+                        width={this.state.documentWidth}
+                        height="750px"
+                        frameBorder="0" 
+                        marginHeight="0" 
+                        marginWidth="0"
+                    >
+                        {/* <div className="freebirdFormviewerViewFormContentWrapper"></div> */}
+                        Loading…
+                    </iframe>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Questionnaire;
